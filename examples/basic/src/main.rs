882f2964f7109572b6b789202af5535ebd22d05b
Kyle <k00benne@gmail.com>
2023-06-19 03:50:18 -0400

    feat: example

--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use revolt_rs::{http::client::Client, gateway::WebSocketClient};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()>   {
+    tracing_subscriber::fmt::init(); 
+
+    let client = Client::builder()
+    .token(env::var("REVOLT_TOKEN")?)
+    .is_bot(Some(false))
+    .exec(); 
+
+    let user = client.fetch_user("01H2VSNEK6A2F5JAMB5GE6R94E").await?; 
+
+    println!("Hello, {:?}!", user.username);
+    
+    Ok(())
 }
