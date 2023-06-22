af0f1dfe5970b44a71e3307a3d9d090423813ace
Kyle <k00benne@gmail.com>
2023-06-22 02:54:48 -0400

    Update README

--- crates/revolt-rs-gateway/src/lib.rs
+++ crates/revolt-rs-gateway/src/lib.rs
@@ -6,12 +6,12 @@ use tokio_tungstenite::{connect_async, tungstenite::Message, MaybeTlsStream, Web
 use tracing::info;
 
 #[derive(Debug)]
+#[warn(dead_code)]
+/// A websocket client for the Revolt API.
 pub struct WebSocketClient {
     stream: WebSocketStream<MaybeTlsStream<TcpStream>>,
     heartbeat_interval: Duration,
-    pong_timeout: Duration, 
-    // websocket_domain: String,
-    // token: String,
+    pong_timeout: Duration,
 }
 
 const BONFIRE_URL: &str = "wss://ws.revolt.chat/?format=msgpack";
@@ -36,11 +36,9 @@ pub enum Events {
     },
 }
 
-// TOOD: support self-hosted revolt instances.
 impl WebSocketClient {
+    /// Connect to the official revolt instance.
     pub async fn connect() -> Result<WebSocketClient, Box<dyn std::error::Error + Send + Sync>> {
-        // let _ = self.close().await;
-
         let (stream, _) = connect_async(BONFIRE_URL)
             .await
             .expect("Connection to websocket failed");
@@ -52,6 +50,20 @@ impl WebSocketClient {
         })
     }
 
+    /// Connect to a self-hosted revolt instance.
+    pub async fn connect_with_url(url: &str) -> Result<WebSocketClient, Box<dyn std::error::Error + Send + Sync>> {
+        let (stream, _) = connect_async(url.to_string())
+            .await
+            .expect("Connection to websocket failed");
+
+        Ok(Self {
+            stream,
+            heartbeat_interval: Duration::from_secs(30),
+            pong_timeout: Duration::from_secs(10),
+        })
+    }
+
+    /// Send a message to the websocket.
     pub async fn send(&mut self, event: Events) -> Result<(), ()> {
         let msg = Message::Binary(rmp_serde::to_vec(&event).unwrap());
 
@@ -60,6 +72,7 @@ impl WebSocketClient {
         Ok(())
     }
 
+    /// Close the websocket connection.
     pub async fn close(&mut self) -> Result<(), ()> {
         info!(target: "WS", "Closing the connection");
 
--- examples/basic/src/main.rs
+++ examples/basic/src/main.rs
@@ -1,19 +1,36 @@
 use std::env;
 
-use revolt_rs::{http::client::Client, gateway::WebSocketClient};
+use revolt_rs::{
+    http::client::Client,
+    model::{channel::message::embed::EmbedBuilder, id::Id},
+};
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()>   {
-    tracing_subscriber::fmt::init(); 
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
 
     let client = Client::builder()
-    .token(env::var("REVOLT_TOKEN")?)
-    .is_bot(Some(false))
-    .exec(); 
+        .token(env::var("TOKEN")?)
+        .is_bot(false)
+        .exec();
 
-    let user = client.fetch_user("01H2VSNEK6A2F5JAMB5GE6R94E").await?; 
+    let channel_id = Id::new("01H2Y55Y9BMGNQQJTKQJ3SVKTV");
 
-    println!("Hello, {:?}!", user.username);
-    
+    let embed = EmbedBuilder::new()
+        .title("Hello world")
+        .build();
+
+    client
+        .create_message(channel_id)
+        .await
+        .content("Hello world")?
+        .embeds(embed)?
+        .send()
+        .await;
+
+    let me = client.current_user().await?;
+
+    println!("{:?}", me.tag());
+
     Ok(())
 }
